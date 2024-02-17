706ada6059726a75dff537d5bd93481ddb20a3c3
kolia.kravchenko19 <kolia.kravchenko19@gmail.com>
2024-02-17 18:35:45 +0200

    homework 15

--- sort.php
+++ sort.php
@@ -1,10 +1,26 @@
 <?php
 
-//Сортування вибором
+
 
 $unsorted = [23, 3, 5, 34, 2, 11, 35, 796, 0, 44];
 
+//Пошук найменшого числа
 
+function Minimal_element(array $array)
+{
+    $min=$array[0];
+    for($i=0; $i<count($array); $i++){
+        if ($array[$i]<$min) $min=$array[$i];
+    }
+    return $min;
+}
+
+echo 'Minimal number: '. PHP_EOL;
+echo Minimal_element($unsorted) . PHP_EOL;
+
+
+
+//Сортування вибором
 function Selection_Sort(array $array): array
 {
     for ($i=0; $i<count($array); $i++)
@@ -24,5 +40,12 @@ function Selection_Sort(array $array): array
 
     return $array;
 }
-
+echo 'Sorted array: '. PHP_EOL;
 echo implode(', ', Selection_Sort($unsorted)) . PHP_EOL;
+
+
+
+
+
+
+
